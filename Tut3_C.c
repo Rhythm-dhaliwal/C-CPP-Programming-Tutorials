52270ae3cf30a86d5480e83cf521d71d7c4a9da2
Rhythm Dhaliwal <rhythmdhaliwal93@gmail.com>
2023-11-06 16:49:13 +0530

    Added Tut3 in the repository

---
+++ Tut3_C.c
@@ -0,0 +1,23 @@
+// IN THIS PROGRAM WE WILL LEARN ABOUT AIRTHMATIC OPERATORS IN C.
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("ENTER THE 1st NUMBER:");
+    scanf("%d",&a);
+       
+
+    printf("ENTER THE 2nd NUMBER:");
+    scanf("%d",&b);
+    
+
+    //USING THE AIRTHMATIC OPERATORS IN C.
+    //EXAMPLE OF AIRTHMATIC OPERATORS ARE +,-,*,/.
+    printf("ADDITON: %d\n",a+b);
+    printf("SUBTRACT: %d\n",a-b);
+    printf("MULTIPLY: %d\n",a*b);
+    printf("DIVIDE: %d\n",a/b);
+
+    return 0;
+}
